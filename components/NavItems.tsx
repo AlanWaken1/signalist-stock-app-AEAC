7ad5247fbd656e5ae24644bdf69e2c30c06d9c1e
Alan Calderón <126179486+AlanWaken1@users.noreply.github.com>
2025-11-13 15:31:47 -0600

    Fix active path check for root route

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -9,7 +9,7 @@ const NavItems = () => {
     const pathname = usePathname();
 
     const isActive = (path: string) => {
-        if (path === '/') return path === '/';
+        if (path === '/') return pathname === '/';
 
         return pathname.startsWith(path);
     }
